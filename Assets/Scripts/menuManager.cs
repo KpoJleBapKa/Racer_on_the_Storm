89d41391a24e4bbb13fea3803d8bfb820a833642
KpoJleBapKa <ars05@ukr.net>
2023-12-01 09:53:13 +0200

    v0.1 car, map

---
+++ Assets/Scripts/menuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuManager : MonoBehaviour
+{
+
+    public GameObject settingsPanel;
+
+    public void PlayGame()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SettingsPanel()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+}
+
