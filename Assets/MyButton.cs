74f1e9a1434db5dcd6a29b86c083a185b4a4d39f
KpoJleBapKa <ars05@ukr.net>
2024-01-11 11:48:28 +0200

    0.7 pre-release

---
+++ Assets/MyButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class MyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    public Image BackGroundImage;
+
+    private void Start()
+    {
+        BackGroundImage.color = new Color(0, 0, 0, 0);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        BackGroundImage.color = new Color(0, 0, 0, 0.4f);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        BackGroundImage.color = new Color(0, 0, 0, 0.1f);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        BackGroundImage.color = new Color(0, 0, 0, 0.1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        BackGroundImage.color = new Color(0, 0, 0, 0);
+    }
+}
